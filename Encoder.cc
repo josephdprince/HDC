2a49fe0925b8f37014a644dfc534cd6e92fe0943
Hong Dong <59760823+hongdong1220@users.noreply.github.com>
2022-10-15 22:09:42 -0700

    started translating encoder

--- Encoder.cc
+++ Encoder.cc
@@ -1,1 +1,12 @@
-//YO
+/*
+Implementation of the encoder in c++ from onlinehd: https://github.com/BIASLab-UCI/onlinehd/blob/master/onlinehd/encoder.py
+"Translated" by: Hong Dong
+Last Updated: 10/15/2022
+*/
+
+#include "Encoder.h";
+
+Encoder::Encoder(int features, int dim = 4000){
+    this->dim = dim;
+    this->num_featuers = features;
+}
---
+++ Encoder.h
@@ -0,0 +1,16 @@
+/*
+Implementation of the encoder in c++ from onlinehd: https://github.com/BIASLab-UCI/onlinehd/blob/master/onlinehd/encoder.py
+"Translated" by: Hong Dong
+Last Updated: 10/15/2022
+*/
+
+class Encoder{
+    private:
+        int dim, num_featuers;
+        //basis, base - pytorch related vars
+    public:
+        Encoder(int features, int dim = 4000);
+
+        //pytorch related function
+        Encoder to();
+};
