f1a73d24cf6e394af637cd02026f59afe79f9c15
josephdprince <joseph.d.prince@me.com>
2022-11-01 10:43:14 -0700

    Finish training

--- HDC.c
+++ HDC.c
@@ -5,19 +5,39 @@
 #include <stdlib.h>
 #include <time.h>
 
+void populateBasis(struct BasisVectors *target) {
+  for (int i = 0; i < DIMENSIONS; i++) {
+    rng_gen(&(target->b_vectors[i]));
+  }
+}
+
+void encode(struct HDvector *hdc, struct BasisVectors *basis,
+            struct ENvector *encoded) {
+  encoded->magnitude = 0;
+  for (int i = 0; i < DIMENSIONS; ++i) {
+    for (int j = 0; j < FEATURES; ++j) {
+      encoded->vector[i] = hdc->vector[j] * basis->b_vectors[i].vector[j];
+      encoded->magnitude += powf(encoded->vector[i], 2);
+    }
+  }
+  encoded->magnitude = sqrtf(encoded->magnitude);
+}
+
 void train(struct classList *l, int numClass, struct ENvector *sample) {
   // Is a float large enough?
   float magnitude = 0;
   for (int i = 0; i < DIMENSIONS; ++i) {
-    l->classes[numClass][i] += sample->vector[i];
-    magnitude += powf(l->classes[numClass][i], 2);
+    l->classes[numClass].vector[i] += sample->vector[i];
+    magnitude += powf(l->classes[numClass].vector[i], 2);
   }
   magnitude = sqrtf(magnitude);
+  l->classes[numClass].magnitude = magnitude;
+}
 
+void normalize(struct classList *l, int numClass) {
   for (int i = 0; i < DIMENSIONS; ++i) {
-    l->classes[numClass][i] /= magnitude;
+    l->classes[numClass].vector[i] /= l->classes[numClass].magnitude;
   }
-  
 }
 
 float float_rand(float min, float max) {
@@ -26,52 +46,80 @@ float float_rand(float min, float max) {
   return min + scale * (max - min);              /* [min, max] */
 }
 
-struct HDvector *rng_gen(struct HDvector *target) {
+void rng_gen(struct HDvector *target) {
   // fill the given vector with randeom float # from -1 to 1
-  srand(time(NULL));
-  int length = FEATURES;
+  static char initial_setup = 1;
+  if (initial_setup == 1) {
+    srand(time(NULL));
+    initial_setup = 0;
+  }
   float total = 0;
   float new_float = 0;
 
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < FEATURES; i++) {
     new_float = float_rand(-1.0, 1.0);
     target->vector[i] = new_float;
     total += powf(new_float, 2);
-    // printf ("new float ^2 = %f|| new total = %f\n" , pow(new_float,2),
-    // total);
   }
-  // printf("total^1/2 = %f\n", sqrtf(total));
   target->magnitude = sqrtf(total);
-  return target;
 }
 
-void print_full_vector(struct HDvector *target) {
-  printf("Printing a %i dimension vector w/ a magnitude of %f :\n", FEATURES,
-         target->magnitude);
-  int length = FEATURES;
-  printf("[");
-  for (int i = 0; i < length; i++) {
-    printf("%f", target->vector[i]);
-    if (i != length - 1) {
-      printf(", ");
-    }
+void print_vector(struct HDvector *target, char includeInfo) {
+  // print the full vector if the size is <= 4, otherwise just print partial
+  if (includeInfo) {
+    printf("Printing a %i-wide vector w/ a magnitude of %f :\n", FEATURES,
+           target->magnitude);
   }
-  printf("]\n");
-}
-
-void print_partial_vector(struct HDvector *target) {
-  // only print the first 2 and last 2
-  // assume target.vector size is atleast 2, should be 4
-  printf("Printing a %i Dimension vector w/ a magnitude of %f :\n", FEATURES,
-         target->magnitude);
-  printf("[%f, %f, ..., %f, %f]\n", target->vector[0], target->vector[1],
-         target->vector[FEATURES - 2], target->vector[FEATURES - 1]);
-}
-
-void print_vector(struct HDvector *target) {
   if (FEATURES <= 4) {
     print_full_vector(target);
   } else {
     print_partial_vector(target);
   }
+}
+
+void print_full_vector(struct HDvector *target) {
+  // This function print all the elements in the vector
+  printf("[");
+  for (int i = 0; i < FEATURES; i++) {
+    printf("%f", target->vector[i]);
+    if (i != FEATURES - 1) {
+      printf(", ");
+    }
+  }
+  printf("]");
+}
+
+void print_partial_vector(struct HDvector *target) {
+  // only print the first 2 and last 2
+  // assume target.vector size is atleast 2, should be 4
+  printf("[%f, %f, ..., %f, %f]", target->vector[0], target->vector[1],
+         target->vector[FEATURES - 2], target->vector[FEATURES - 1]);
+}
+
+void print_basis(struct BasisVectors *target) {
+  int top_bot_num = 2; // decide how many elem at the top and at bottom to print
+  printf("Printing a Basis-Vector of %i features by %i dimensions:\n", FEATURES,
+         DIMENSIONS);
+  printf("[\n");
+
+  // Print [0, top_bot_num] of basis
+  for (int i = 0; i < top_bot_num; i++) {
+    printf("\t%i: ", i);
+
+    // print_full_vector(&target->b_vectors[i]);
+    print_vector(&(target->b_vectors[i]), FALSE);
+    printf("\n");
+  }
+  printf("\t.\n\t.\n\t.\n");
+
+  // Print [DIMENSIONS - top_bot_num, DIMENSIONS] of basis
+  for (int i = DIMENSIONS - top_bot_num; i < DIMENSIONS; ++i) {
+    printf("\t%i: ", i);
+
+    // print_full_vector(&target->b_vectors[i]);
+    print_vector(&(target->b_vectors[i]), FALSE);
+    printf("\n");
+  }
+
+  printf("\n]\n");
 }
--- HDC.h
+++ HDC.h
@@ -1,42 +1,71 @@
 #pragma once
 
 /* Global variables */
-#define DIMENSIONS 10000
-#define FEATURES 784
+#define DIMENSIONS 10
+#define FEATURES 28
 #define CLASSES 10
+
+#define FALSE 0
+#define TRUE 1
 
 typedef float FeatType;
 
+/* ============== Different abstract structs ============== */
+
+/* A vector for all the data for each feature*/
 struct HDvector {
+  // To represent a single fecture vector of the basis vector
   FeatType magnitude;
   FeatType vector[FEATURES];
 };
 
+/* An encoded vector */
 struct ENvector {
   FeatType magnitude;
   FeatType vector[DIMENSIONS];
 };
 
+/* Encoded sample of each class */
 struct classList {
-  FeatType classes[CLASSES][DIMENSIONS];
+  struct ENvector classes[CLASSES];
 };
 
+/* Basis vector used for encoding */
+struct BasisVectors {
+  struct HDvector b_vectors[DIMENSIONS];
+};
+
+/* ============== Main fucntions for HDC ============== */
+
 /* Populates each column in the basis matrix with random values from [-1,1] */
-void populateBasis(struct HDvector *hdc);
+void populateBasis(struct BasisVectors *target);
 
 /* Encodes a sample vector by multiplying it with the basis matrix */
-struct ENvector* encode(struct HDvector *hdc);
+void encode(struct HDvector *hdc, struct BasisVectors *basis,
+            struct ENvector *encoded);
 
-/* Encodes a sample vector and adds it to the classList */
+/* Adds an encoded vector to the classList */
 void train(struct classList *l, int numClass, struct ENvector *sample);
+
+/* Normalize ENvectors in classList */
+void normalize(struct classList *l, int numClass);
 
 /* Encodes a sample and compares it to the closest class in classList. Return is
  * a numerical value of the classification */
 int similarity(struct HDvector *hdc, FeatType sample[]);
 
-/* Helper functuins for HDvector */
+/* ============== Helper functions for HDvector ============== */
+
+/* Generate random float number [min, max] */
 float float_rand(float min, float max);
-struct HDvector* rng_gen(struct HDvector *target);
+
+/* Populate target's vector with random FeatType element */
+void rng_gen(struct HDvector *target);
+
+/* Decide to print partial or full vector */
+void print_vector(struct HDvector *target, char includeInfo);
 void print_full_vector(struct HDvector *target);
 void print_partial_vector(struct HDvector *target);
-void print_vector(struct HDvector *target);
+
+/* Decide to print partial of the BasisVector */
+void print_basis(struct BasisVectors *target);
