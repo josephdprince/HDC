7b934e83f674e936e84f5b34ab7630bf6954e506
Hong Dong <59760823+hongdong1220@users.noreply.github.com>
2023-02-06 21:07:39 -0800

    added new file for synthesis

---
+++ src/HDC_HLS.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/* Global variables */
+#define DIMENSIONS 10
+#define FEATURES 28
+#define CLASSES 10
+
+#define FALSE 0
+#define TRUE 1
+
+#define PARTIAL 0
+#define FULL 1
+
+typedef float FeatType;
+
+/*========== Function to Synthesize ==========*/
+
